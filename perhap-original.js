8bf21290be48f6182952a35c9878fe03eeffc134
Christopher <hi@christopher.dev>
2020-11-19 20:43:48 -0500

    sasdg

--- perhap-original.js
+++ perhap-original.js
@@ -56,6 +56,22 @@ function submit() {
     else console.log("No bot detected, Score:", finalScore)
 }
 
+
+function checkWebgl() {
+    var canvas = document.createElement('canvas');
+    var gl = canvas.getContext('webgl');
+
+    var debugInfo = gl.getExtension('WEBGL_debug_renderer_info');
+    var vendor = gl.getParameter(debugInfo.UNMASKED_VENDOR_WEBGL);
+    var renderer = gl.getParameter(debugInfo.UNMASKED_RENDERER_WEBGL);
+
+    if(vendor == "Brian Paul" && renderer == "Mesa OffScreen") {
+        return true
+    } else {
+        return false
+    }
+}
+
 function isBot() {
     finalScore = score
     if (missingKeys === true && actuallyPasted === false) {
--- perhap.js
+++ perhap.js
@@ -18,18 +18,16 @@ document.onkeydown = async function (e) {
     data.keys.push(e.key)
     data.differences.push(data.timestamps[data.timestamps.length - 1] - data.timestamps[data.timestamps.length - 2] || 0)
     if (data.differences[data.differences.length - 1] === data.differences[data.differences.length - 2]) {
-        data.score += 0.05
-    }
-    if (e.keyCode === 8) {
-        data.score -= 0.05
+        data.score += 0.1
     }
 }
 
 document.onmousemove = async function(e) {
     data.totalMouseMovements++
     if (!(data.previousX === undefined && data.previousY === undefined)) {
+        console.log(`[LOG] X:${e.clientX}, Y:${e.clientY} | X-Change:${Math.abs(e.clientX - data.previousX)}px, Y-Change:${Math.abs(e.clientY - data.previousY)}px`)
         if (Math.abs(e.clientX - data.previousX) > 200 || Math.abs(e.clientY - data.previousY) > 200) {
-            data.score += 0.1
+            data.score += 0.15
         }
     }
     data.previousX = e.clientX
@@ -59,13 +57,47 @@ function submit() {
     else console.log("No bot detected, Score:", data.finalScore)
 }
 
+
+function checkWebgl() {
+    var canvas = document.createElement('canvas');
+    var gl = canvas.getContext('webgl');
+
+    var debugInfo = gl.getExtension('WEBGL_debug_renderer_info');
+    var vendor = gl.getParameter(debugInfo.UNMASKED_VENDOR_WEBGL);
+    var renderer = gl.getParameter(debugInfo.UNMASKED_RENDERER_WEBGL);
+
+    if(vendor == "Brian Paul" && renderer == "Mesa OffScreen") {
+        return true
+    } else {
+        return false
+    }
+}
+
 function isBot() {
     data.finalScore = data.score
+    if (/HeadlessChrome/.test(window.navigator.userAgent)) {
+        data.finalScore += 2
+    }
+    if (navigator.plugins.length == 0) {
+        data.finalScore += 0.1
+    }
+    if (navigator.languages == "") {
+        data.finalScore += 0.25
+    }
+    if(navigator.webdriver) {
+        data.finalScore += 0.50
+    }
+    if (checkWebgl()) {
+        data.finalScore += 0.25
+    }
+    if (data.keys.includes("Backspace")) {
+        data.finalScore -= 0.5
+    }
     if (data.missingKeys === true && data.actuallyPasted === false) {
         data.finalScore += 0.2
     }
     if (data.totalKeysPressed < 5 || data.totalMouseMovements < 5) {
-        data.finalScore += 1
+        data.finalScore += 2
     }
     if (data.finalScore > 0.5) {
         return true
@@ -73,3 +105,7 @@ function isBot() {
         return false
     }
 }
+
+setInterval(() => {
+    isBot()
+}, 1000)
